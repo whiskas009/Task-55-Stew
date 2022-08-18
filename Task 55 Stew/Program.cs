b44e425d8b6b603923f5620c767fbc471b9ecdc2
Daniil Gorin <daniil009.gorin@yandex.ru>
2022-08-18 19:11:38 +0300

    Добавьте файлы проекта.

---
+++ Task 55 Stew/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_55_Stew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<CanStew> cansStew = new List<CanStew>(){
+            new CanStew("Высший сорт", 2001, 2011),
+            new CanStew("Высший сорт", 2002, 2012),
+            new CanStew("Высший сорт", 2003, 2013),
+            new CanStew("Белорусская", 2005, 2025),
+            new CanStew("Белорусская", 2005, 2025),
+            new CanStew("Белорусская", 2005, 2025),
+            new CanStew("Гродфуд", 2006, 2015),
+            new CanStew("Гродфуд", 2006, 2015),
+            new CanStew("Гродфуд", 2007, 2016),
+            new CanStew("Гродфуд", 2008, 2017)};
+
+            int currentYear = 2020;
+
+            var overdueStew = cansStew.Where(canStew => canStew.ExpirationDate < currentYear);
+
+            Console.WriteLine($"Текущий год: {currentYear}\n");
+            Console.WriteLine("Просроченная тушенка: \n");
+
+            foreach (var canStew in overdueStew)
+            {
+                canStew.ShowInfo();
+            }
+        }
+    }
+
+    class CanStew
+    {
+        public string Name { get; private set; }
+        public int YearProduction { get; private set; }
+        public int ExpirationDate { get; private set; }
+
+        public CanStew(string name, int yearProduction, int expirationDate)
+        {
+            Name = name;
+            YearProduction = yearProduction;
+            ExpirationDate = expirationDate;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Тушенка: {Name}. Год выпуска: {YearProduction}. Срок годности: {ExpirationDate}.");
+        }
+    }
+}
---
+++ Task 55 Stew/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Task 55 Stew")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task 55 Stew")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c49a057a-0d53-4eac-aea7-99e0b9936449")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
